c3b62ff1bceeb6cc716206b0aec364d05c7016e7
vitalyailinskyi <lamborhgini.888@gmail.com>
2014-11-18 20:32:31 +0200

    Changes in Main.java

--- Calculator/src/Main/Main.java
+++ Calculator/src/Main/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Frame f = new Calculator("Ильинский Виталий - Калькулятор");
+		Frame f = new Calculator("Ilinskyi Vitalii - Calculator");
 		f.setVisible(true);
 		f.setBounds(7, 7, 400, 260);
 		f.setResizable(false);
 	}
-} /* Окончание класса Main */ 
+} /* End of class Main */ 
 
 
  
